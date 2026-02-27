d8bd1c04325673e9f85531c8b8aa0b9c6e98df95
Hasti <hsati.ranjkesh@gmail.com>
2026-02-27 10:45:18 +0100

    Add "TO-DO: #" button.

--- Exercise/ViewController.swift
+++ Exercise/ViewController.swift
@@ -13,17 +13,28 @@ class ViewController: UIViewController {
 
 		view.backgroundColor = .white
 
-		let button = UIButton()
-		button.translatesAutoresizingMaskIntoConstraints = false
-		button.setTitle("About", for: .normal)
-		button.setTitleColor(.systemBlue, for: .normal)
-		button.addTarget(self, action: #selector(aboutButtonTap), for: .touchUpInside)
+		let todoButton = UIButton(type: .system)
+		todoButton.translatesAutoresizingMaskIntoConstraints = false
+		todoButton.setTitle("TO-DO: #", for: .normal)
+		todoButton.setTitleColor(.systemBlue, for: .normal)
 
-		view.addSubview(button)
+		let aboutButton = UIButton(type: .system)
+		aboutButton.translatesAutoresizingMaskIntoConstraints = false
+		aboutButton.setTitle("About", for: .normal)
+		aboutButton.setTitleColor(.systemBlue, for: .normal)
+		aboutButton.addTarget(self, action: #selector(aboutButtonTap), for: .touchUpInside)
+
+		let stackView = UIStackView(arrangedSubviews: [todoButton, aboutButton])
+		stackView.translatesAutoresizingMaskIntoConstraints = false
+		stackView.axis = .vertical
+		stackView.alignment = .center
+		stackView.spacing = 16
+
+		view.addSubview(stackView)
 
 		NSLayoutConstraint.activate([
-			button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+			stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			stackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
 		])
 	}
 
